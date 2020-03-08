53d5043abe1df2ceddb94bfd1de5197dcefbf15f
YaliW <wangyalijob@163.com>
2020-03-08 22:30:56 +0800

    feat: add random

--- Design/伪随机.js
+++ Design/伪随机.js
@@ -1,59 +1,36 @@
-var json = {
-    userId: 123,
-    // 根据 UI 页面确定需要的用户信息
-    userName: 'user name',
-    userEmail: 'user email',
-    // 获奖的信息
-    prize: {
-        id: 1,
-        prizeProduct: 'Iphone',
-        price: '8000'
-    }
-}
 
-function getPrize() {
+// 伪随机
+function getRandomPrize(prizeArr) {
     const random = Math.random() / 100;
-    let res = '';
+    const probArr = prizeArr.reduce((prev, curr) => {
+        const remains = prev.length === 0 ? 0 : prev[prev.length - 1];
+        prev.push(remains + (curr.count === 0 ? 0 : curr.prob));
+        return prev;
+    }, []);
 
-    if (random <= 0.0005 && random > 0) {
-        res = '1';
-    } else if (random <= 0.001 && random > 0.005) {
-        res = '2';
-    } else if (random <= 0.002 && random > 0.001) {
-        res = '3';
-    } else if (random <= 0.004 && random > 0.002) {
-        res = '4';
-    } else {
-        res = 'sorry';
-    }
-    
-    return res;
-}
-
-const result = getPrize();
-// console.log(result)
-
-prizeArr = [1, 2, 3, 4];
-probArr = [0.0005, 0.001, 0.002, 0.004];
-
-function getRandomPrize(prizeArr, probArr) {
-    let sum = 0;
-    let prev = 0;
-    let random = Math.random() /100;
     const len = probArr.length;
 
-    for (let i=len -1; i>=0; i--) {
-        sum = probArr[i] + sum;
-    }
-
-    for (let i=len-1; i>=0; i--) {
-        prev = probArr[i] + prev;
-        if (random <= prev) {
-            return prizeArr[i];
+    for (let i=0; i<len; i++) {
+        if (random <= probArr[i]) {
+            prizeArr[i].count--;
+            return prizeArr[i].type;
         }
     }
     return 'sorry';
 };
 
-const a = getRandomPrize(prizeArr, probArr);
-console.log(a);
+function Main() {
+    const prizeArr = [
+        { type: 1, count: 1, prob: 0.0005 }, 
+        { type: 2, count: 2, prob: 0.0010 }, 
+        { type: 3, count: 3, prob: 0.002 }, 
+        { type: 4, count: 4, prob: 0.004 }
+    ];
+    const result = [];
+    for (let i=0; i < 5000; i++) {
+        result.push(getRandomPrize(prizeArr));
+    }
+    console.log(result.filter(item => item !== 'sorry'), result.length);
+}
+
+Main();
