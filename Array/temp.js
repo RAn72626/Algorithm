4573e229389c44e0650c0c87872b1a27e0027404
YaliW <wangyalijob@163.com>
2020-03-15 18:57:02 +0800

    update

--- Array/temp.js
+++ Array/temp.js
@@ -1,52 +1,20 @@
-let array = [[1,2,2],[3,4,5,5],[6,7,8,9,[11,12,[12,13,[14]]]],10];
-
-function handler(array) {
-    let newArray = array.join(',').split(',');
-    let set = unique(newArray);
-    quickSort(set, 0, set.length-1);
-    return set;
+function maxSubarray(array) {
+    if (array.length === 0) {
+        return 0;
+    } else {
+        let res = array[0], sum = 0;
+        for (let item of array) {
+            if (sum > 0) {
+                sum += item;
+            } else {
+                sum = item;
+            }
+            res = Math.max(sum, res);
+        }
+        return res;
+    }
 }
 
-const res = handler(array);
+let array = [-2,1,-3,4,-1,2,1,-5,4];
+const res = maxSubarray(array);
 console.log(res)
-
-function unique(array) {
-    let obj = {};
-    for (let value of array) {
-        if (obj.hasOwnProperty(value)) {
-            obj[value]++;
-        } else {
-            obj[value] = 1;
-        }
-    }
-    return Object.keys(obj).map(item => parseInt(item));
-}
-
-function quickSort(array, left, right) {
-    if (left >= right) {
-        return;
-    }
-
-    let currVal = array[left];
-    let leftIndex = left, rightIndex = right;
-
-    while (leftIndex < rightIndex) {
-        while (leftIndex < rightIndex) {
-            if (array[rightIndex] < currVal) {
-                array[leftIndex] = array[rightIndex];
-                break;
-            }
-            rightIndex--;
-        }
-        while (leftIndex < rightIndex) {
-            if (array[leftIndex] > currVal) {
-                array[rightIndex] = array[leftIndex];
-                break;
-            }
-            leftIndex++;
-        }
-    }
-    array[leftIndex] = currVal;
-    quickSort(array, left, leftIndex-1);
-    quickSort(array, rightIndex+1, right);
-}
